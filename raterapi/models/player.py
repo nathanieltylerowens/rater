3a10e8318a51f5303f818e3b4f0c357916a5db20
NateOwens <nathanieltylerowens@gmail.com>
2021-01-24 18:32:19 -0600

    added fixtures and fix files to make work

--- raterapi/models/player.py
+++ raterapi/models/player.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.deletion import CASCADE
 
 class Player(models.Model):
-    name = models.CharField(max_length=20)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=CASCADE, default=1)
