671a01a3888ca24bad8bb92eae375c5248a8d53a
NateOwens <nathanieltylerowens@gmail.com>
2021-02-06 13:10:25 -0600

    fixing aaaaaaalll the mistakes

--- raterreports/views/__init__.py
+++ raterreports/views/__init__.py
@@ -3,7 +3,7 @@ from .connection import Connection
 from .games.top_five_games import top_five_games
 from .games.bottom_five_games import bottom_five_games
 from .games.games_per_category import games_per_category
-from .games.games_three_players import more_than_three
+from .games.more_than_three import more_than_three
 from .games.most_reviewed import most_reviewed_game
 from .games.under_eight_games import under_eight_games
 from .games.top_three_reviewers import top_three_reviewers
