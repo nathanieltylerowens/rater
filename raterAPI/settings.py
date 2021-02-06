6a6c3d03d87b06abd49b05c641f595d1fc881ec7
NateOwens <nathanieltylerowens@gmail.com>
2021-02-06 13:07:39 -0600

    added step 6 to settings

--- raterAPI/settings.py
+++ raterAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,7 +140,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+
+django_on_heroku.settings(locals())
 
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
